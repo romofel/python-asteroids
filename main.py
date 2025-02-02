355af9c2487c1f93c30196c2f6308f36e146f629
romofel <felipe@romofel.dev>
2025-02-01 19:22:00 -0600

    display window and handle close events

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import pygame
+from constants import *
+
+BLACK = (0, 0, 0)
 
 def main():
+    pygame.init()
     print("Starting asteroids!")
+    screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(BLACK)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
